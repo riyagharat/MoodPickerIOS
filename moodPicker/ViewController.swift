38237f4d8662356239341ba450d7375859021bac
riya <riya@sumeets-mbp.hsd1.fl.comcast.net>
2017-09-08 14:41:38 -0400

    Initial commit

--- moodPicker/ViewController.swift
+++ moodPicker/ViewController.swift
@@ -8,7 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    let moodlist:[String] = ["Ecstatic", "Happy", "Cheerful", "Fine", "Tired", "Maudlin", "Depressed", "Overwhelmed"]
+    
+    //delegate
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return moodlist[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        var color:UIColor
+        switch row {
+        case 0, 1, 2:
+            color = UIColor.yellow
+        case 3, 4, 5:
+            color = UIColor.blue
+        case 6, 7:
+            color = UIColor.brown
+        default:
+            color = UIColor.red
+        }
+        self.view.backgroundColor = color
+    }
+    
+    
+    // data source
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return moodlist.count
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
